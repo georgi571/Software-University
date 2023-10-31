8779a2ea09f599015cb7aa12a6e6f6b50d98509c
Georgi Kostadinov <georgi93571@gmail.com>
2023-10-31 20:04:45 +0200

    add solution

--- src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_02WordSynonyms.java
+++ src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_02WordSynonyms.java
@@ -1,9 +1,20 @@
 package _2ProgrammingFundamentalsWithJavaSeptember2023._1JavaFundamentals._07AssociativeArrays;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class _02WordSynonyms {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        LinkedHashMap<String, List<String>> synonyms = new LinkedHashMap<>();
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            String word = scanner.nextLine();
+            String synonym = scanner.nextLine();
+            synonyms.putIfAbsent(word, new ArrayList<>());
+            synonyms.get(word).add(synonym);
+        }
+        for (Map.Entry<String, List<String>> kvp : synonyms.entrySet()) {
+            System.out.printf("%s - %s%n", kvp.getKey(), String.join(", ", kvp.getValue()));
+        }
     }
 }
