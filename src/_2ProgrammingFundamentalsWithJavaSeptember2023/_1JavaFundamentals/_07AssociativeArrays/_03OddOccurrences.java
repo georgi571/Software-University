17e7340fa5376364919d0d47bfcad30ef40d4fe5
Georgi Kostadinov <georgi93571@gmail.com>
2023-10-31 20:52:27 +0200

    add solution

--- src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_03OddOccurrences.java
+++ src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_03OddOccurrences.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class _03OddOccurrences {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
     }
 }
--- src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_04WordFilter.java
+++ src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_04WordFilter.java
@@ -1,9 +1,16 @@
 package _2ProgrammingFundamentalsWithJavaSeptember2023._1JavaFundamentals._07AssociativeArrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class _04WordFilter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] words = Arrays.stream(scanner.nextLine().split(" "))
+                .filter(word -> word.length() % 2 == 0)
+                .toArray(String[]::new);
+        for (String word : words) {
+            System.out.printf("%s%n", word);
+        }
     }
 }
