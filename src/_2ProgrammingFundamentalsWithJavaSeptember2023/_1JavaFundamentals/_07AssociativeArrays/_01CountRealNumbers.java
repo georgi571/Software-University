7ab33c7f02baf942d17052e3badc0f9ea57d9620
Georgi Kostadinov <georgi93571@gmail.com>
2023-10-31 19:55:58 +0200

    add solution

---
+++ src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_01CountRealNumbers.java
@@ -0,0 +1,28 @@
+package _2ProgrammingFundamentalsWithJavaSeptember2023._1JavaFundamentals._07AssociativeArrays;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class _01CountRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TreeMap<Double, Integer> numbersCount = new TreeMap<>();
+        double[] nums = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToDouble(Double::parseDouble)
+                .toArray();
+        for (double num : nums) {
+            if (numbersCount.containsKey(num)) {
+                numbersCount.put(num, numbersCount.get(num) + 1);
+            } else {
+                numbersCount.put(num, 1);
+            }
+        }
+        for (Map.Entry<Double, Integer> kvp : numbersCount.entrySet()) {
+            String keyFormat = new DecimalFormat("#.####").format(kvp.getKey());
+            System.out.printf("%s -> %d%n", keyFormat, kvp.getValue());
+        }
+    }
+}
---
+++ src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_04WordFilter.java
@@ -0,0 +1,9 @@
+package _2ProgrammingFundamentalsWithJavaSeptember2023._1JavaFundamentals._07AssociativeArrays;
+
+import java.util.Scanner;
+
+public class _04WordFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
