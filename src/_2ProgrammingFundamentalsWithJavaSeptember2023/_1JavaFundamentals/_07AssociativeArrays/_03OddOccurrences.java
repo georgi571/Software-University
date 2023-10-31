3c12a04ec09a5ba3919cfbf99fef52256a3dbd13
Georgi Kostadinov <georgi93571@gmail.com>
2023-10-31 21:13:10 +0200

    add solution

--- src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_03OddOccurrences.java
+++ src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_03OddOccurrences.java
@@ -1,10 +1,29 @@
 package _2ProgrammingFundamentalsWithJavaSeptember2023._1JavaFundamentals._07AssociativeArrays;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class _03OddOccurrences {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String[] words = Arrays.stream(scanner.nextLine().split(" "))
+                .map(word -> word.toLowerCase())
+                .toArray(String[]::new);
+        LinkedHashMap<String, Integer> wordsMap = new LinkedHashMap<>();
+        for (String word : words) {
+            if (wordsMap.containsKey(word)) {
+                wordsMap.put(word, wordsMap.get(word) + 1);
+            } else {
+                wordsMap.put(word, 1);
+            }
+        }
+        String[] result = wordsMap
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() % 2 != 0)
+                .map(entry -> entry.getKey())
+                .toArray(String[]::new);
+        System.out.println(String.join(", ", result));
     }
 }
