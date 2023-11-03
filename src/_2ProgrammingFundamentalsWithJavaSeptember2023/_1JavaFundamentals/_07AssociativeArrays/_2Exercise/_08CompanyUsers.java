b23075f137c322dfcae9d2a41bff48314cc157e4
Georgi Kostadinov <georgi93571@gmail.com>
2023-11-03 20:49:46 +0200

    add solution

---
+++ src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_2Exercise/_08CompanyUsers.java
@@ -0,0 +1,30 @@
+package _2ProgrammingFundamentalsWithJavaSeptember2023._1JavaFundamentals._07AssociativeArrays._2Exercise;
+
+import java.util.*;
+
+public class _08CompanyUsers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map <String, List<String>> companyEmployees = new LinkedHashMap<>();
+        String command = scanner.nextLine();
+        while (!command.equals("End")) {
+            String[] input = command.split(" -> ");
+            String companyName = input[0];
+            String id = input[1];
+            if (!companyEmployees.containsKey(companyName)) {
+                companyEmployees.put(companyName, new ArrayList<>());
+                companyEmployees.get(companyName).add(id);
+            } else {
+                List<String> currentID =  companyEmployees.get(companyName);
+                if (!currentID.contains(id)) {
+                    companyEmployees.get(companyName).add(id);
+                }
+            }
+            command = scanner.nextLine();
+        }
+        companyEmployees.entrySet().forEach(entry -> {
+            System.out.printf("%s%n", entry.getKey());
+            entry.getValue().forEach(id -> System.out.printf("-- %s%n", id));
+        });
+    }
+}
