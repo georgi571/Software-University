14de4fe3bdf41793c9c26bbe9a2401be6548111c
Georgi Kostadinov <georgi93571@gmail.com>
2023-11-03 19:47:05 +0200

    add solution

---
+++ src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_2Exercise/_01CountCharsInAString.java
@@ -0,0 +1,25 @@
+package _2ProgrammingFundamentalsWithJavaSeptember2023._1JavaFundamentals._07AssociativeArrays._2Exercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class _01CountCharsInAString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        Map<Character, Integer> symbolsCount = new LinkedHashMap<>();
+        for (char symbol : text.toCharArray()) {
+            if (symbol == ' ') {
+                continue;
+            }
+            if (!symbolsCount.containsKey(symbol)) {
+                symbolsCount.put(symbol, 1);
+            } else {
+                int currentCount = symbolsCount.get(symbol);
+                symbolsCount.put(symbol, currentCount + 1);
+            }
+        }
+        symbolsCount.entrySet().forEach(entry -> System.out.println(entry.getKey() + " -> " + entry.getValue()));
+    }
+}
