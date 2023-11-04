34dcd5d275b3d6f1b1dce341afcb4602a4913b73
Georgi Kostadinov <georgi93571@gmail.com>
2023-11-04 21:26:56 +0200

    add solution

---
+++ src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_2Exercise/_07LegendaryFarming.java
@@ -0,0 +1,56 @@
+package _2ProgrammingFundamentalsWithJavaSeptember2023._1JavaFundamentals._07AssociativeArrays._2Exercise;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class _07LegendaryFarming {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Integer> materials = new LinkedHashMap<>();
+        materials.put("shards", 0);
+        materials.put("fragments", 0);
+        materials.put("motes", 0);
+        boolean isLegendaryItemFind = false;
+        while (!isLegendaryItemFind) {
+            List<String> items = Arrays.stream(scanner.nextLine()
+                                    .toLowerCase()
+                                    .split(" "))
+                            .collect(Collectors.toList());
+            for (int i = 0; i < items.size(); i++) {
+                int quantity = Integer.parseInt(items.get(i));
+                i++;
+                String material = items.get(i);
+                if (!materials.containsKey(material)) {
+                    materials.put(material, quantity);
+                } else {
+                    materials.put(material, materials.get(material) + quantity);
+                }
+                if (materials.containsKey("shards")) {
+                    if (materials.get("shards") >= 250) {
+                        System.out.printf("Shadowmourne obtained!%n");
+                        materials.put(material, materials.get(material) - 250);
+                        isLegendaryItemFind = true;
+                        break;
+                    }
+                }
+                if (materials.containsKey("fragments")) {
+                    if (materials.get("fragments") >= 250) {
+                        System.out.printf("Valanyr obtained!%n");
+                        materials.put(material, materials.get(material) - 250);
+                        isLegendaryItemFind = true;
+                        break;
+                    }
+                }
+                if (materials.containsKey("motes")) {
+                    if (materials.get("motes") >= 250) {
+                        System.out.printf("Dragonwrath obtained!%n");
+                        materials.put(material, materials.get(material) - 250);
+                        isLegendaryItemFind = true;
+                        break;
+                    }
+                }
+            }
+        }
+        materials.entrySet().forEach(entry -> System.out.printf("%s: %d%n", entry.getKey(), entry.getValue()));
+    }
+}
