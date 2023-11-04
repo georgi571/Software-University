5c2a4d12f42bfdc59228c8db3c40d6e8df22f05e
Georgi Kostadinov <georgi93571@gmail.com>
2023-11-04 17:08:59 +0200

    add solution

---
+++ src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_2Exercise/_04SoftUniParking.java
@@ -0,0 +1,37 @@
+package _2ProgrammingFundamentalsWithJavaSeptember2023._1JavaFundamentals._07AssociativeArrays._2Exercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class _04SoftUniParking {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, String> carOwner = new LinkedHashMap<>();
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 1; i <= n ; i++) {
+            String[] input = scanner.nextLine().split(" ");
+            String command = input[0];
+            String username = input[1];
+            if (command.equals("register")) {
+                String licensePlateNumber = input[2];
+                if (!carOwner.containsKey(username)){
+                    carOwner.put(username, licensePlateNumber);
+                    System.out.printf("%s registered %s successfully%n", username,licensePlateNumber);
+                } else {
+                    System.out.printf("ERROR: already registered with plate number %s%n", carOwner.get(username));
+                }
+
+            } else if (command.equals("unregister")) {
+                if (carOwner.containsKey(username)) {
+                    carOwner.remove(username);
+                    System.out.printf("%s unregistered successfully%n", username);
+                } else {
+                    System.out.printf("ERROR: user %s not found%n",username);
+                }
+
+            }
+        }
+        carOwner.entrySet().forEach(entry -> System.out.printf("%s => %s%n", entry.getKey(), entry.getValue()));
+    }
+}
