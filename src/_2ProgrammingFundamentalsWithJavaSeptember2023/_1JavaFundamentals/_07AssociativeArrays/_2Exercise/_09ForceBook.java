65bb12f9a290dfc23d8600d13f70cfe730822599
Georgi Kostadinov <georgi93571@gmail.com>
2023-11-05 13:14:56 +0200

    add solution

---
+++ src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_07AssociativeArrays/_2Exercise/_09ForceBook.java
@@ -0,0 +1,50 @@
+package _2ProgrammingFundamentalsWithJavaSeptember2023._1JavaFundamentals._07AssociativeArrays._2Exercise;
+
+import java.util.*;
+
+public class _09ForceBook {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, List<String>> sides = new LinkedHashMap<>();
+        String command = scanner.nextLine();
+        while (!command.equals("Lumpawaroo")) {
+            if (command.contains("|")) {
+                String[] input = command.split(" \\| ");
+                String side = input[0];
+                String user = input[1];
+                if (!sides.containsKey(side)) {
+                    sides.put(side, new ArrayList<>());
+                }
+                for (Map.Entry<String, List<String>> current : sides.entrySet()) {
+                    if (!current.getValue().contains(user)) {
+                        sides.get(side).add(user);
+                        break;
+                    }
+                }
+            } else if (command.contains("->")) {
+                String[] input = command.split(" -> ");
+                String side = input[1];
+                String user = input[0];
+                for (Map.Entry<String, List<String>> current : sides.entrySet()) {
+                    if (current.getValue().contains(user)) {
+                        sides.get(current.getKey()).remove(user);
+                        break;
+                    }
+                }
+                if (!sides.containsKey(side)) {
+                    sides.put(side, new ArrayList<>());
+                }
+                sides.get(side).add(user);
+                System.out.printf("%s joins the %s side!%n", user, side);
+            }
+            command = scanner.nextLine();
+        }
+        sides.entrySet().forEach(entry -> {
+            int numbers = entry.getValue().size();
+            if (numbers > 0) {
+                System.out.printf("Side: %s, Members: %d%n", entry.getKey(), numbers);
+                entry.getValue().forEach(player -> System.out.printf("! %s%n", player));
+            }
+        });
+    }
+}
