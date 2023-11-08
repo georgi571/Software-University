7df6d21827807e69139e30c5f875dcd55c1640f1
Georgi Kostadinov <georgi93571@gmail.com>
2023-11-08 20:04:13 +0200

    add solution

--- src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_8TextProcessing/_1Lab/_03Substring.java
+++ src/_2ProgrammingFundamentalsWithJavaSeptember2023/_1JavaFundamentals/_8TextProcessing/_1Lab/_03Substring.java
@@ -1,4 +1,21 @@
 package _2ProgrammingFundamentalsWithJavaSeptember2023._1JavaFundamentals._8TextProcessing._1Lab;
 
+import java.util.Scanner;
+
 public class _03Substring {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstString = scanner.nextLine();
+        String secondString = scanner.nextLine();
+        while (true) {
+            int index = secondString.indexOf(firstString);
+            if (index == -1) {
+                break;
+            }
+            String leftString = secondString.substring(0, index);
+            String rightString = secondString.substring(index + firstString.length());
+            secondString = leftString.concat(rightString);
+        }
+        System.out.printf("%s", secondString);
+    }
 }
