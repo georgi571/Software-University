75b57ff47b4e66e4755c19a1575a0532dfbbfce1
Georgi Kostadinov <georgi93571@gmail.com>
2023-12-22 21:16:26 +0200

    add solution

---
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_4StreamsFilesAndDirectories/_1Lab/_06SortLines.java
@@ -0,0 +1,18 @@
+package _3ProgrammingJavaAdvancedJanuary2024._1JavaAdvanced._4StreamsFilesAndDirectories._1Lab;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
+public class _06SortLines {
+    public static void main(String[] args) throws IOException {
+        Path pathRead = Paths.get("D:\\SoftwareUniversity\\Softrware-University\\src\\_3ProgrammingJavaAdvancedJanuary2024\\_1JavaAdvanced\\_4StreamsFilesAndDirectories\\_JavaAdvancedFilesAndStreamsLabResources\\input.txt");
+        Path pathWrite = Paths.get("D:\\SoftwareUniversity\\Softrware-University\\src\\_3ProgrammingJavaAdvancedJanuary2024\\_1JavaAdvanced\\_4StreamsFilesAndDirectories\\_JavaAdvancedFilesAndStreamsLabResources\\06.SortLinesOutput.txt");
+        List<String> allLines = Files.readAllLines(pathRead);
+        Collections.sort(allLines);
+        Files.write(pathWrite, allLines);
+    }
+}
