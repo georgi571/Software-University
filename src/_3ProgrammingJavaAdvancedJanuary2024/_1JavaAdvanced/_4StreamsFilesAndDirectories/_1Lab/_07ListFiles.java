bcbc7312553a6f62778679708c9b4855270579ed
Georgi Kostadinov <georgi93571@gmail.com>
2023-12-22 21:25:24 +0200

    add solution

---
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_4StreamsFilesAndDirectories/_1Lab/_07ListFiles.java
@@ -0,0 +1,19 @@
+package _3ProgrammingJavaAdvancedJanuary2024._1JavaAdvanced._4StreamsFilesAndDirectories._1Lab;
+
+import java.io.File;
+
+public class _07ListFiles {
+    public static void main(String[] args) {
+        File folder = new File("D:\\SoftwareUniversity\\Softrware-University\\src\\_3ProgrammingJavaAdvancedJanuary2024\\_1JavaAdvanced\\_4StreamsFilesAndDirectories\\_JavaAdvancedFilesAndStreamsLabResources\\FilesAndStreams");
+        if (folder.exists()) {
+            if (folder.isDirectory()) {
+                File[] allFiles = folder.listFiles();
+                for (File file : allFiles) {
+                    if (!file.isDirectory()) {
+                        System.out.printf("%s: [%s]%n", file.getName(), file.length());
+                    }
+                }
+            }
+        }
+    }
+}
