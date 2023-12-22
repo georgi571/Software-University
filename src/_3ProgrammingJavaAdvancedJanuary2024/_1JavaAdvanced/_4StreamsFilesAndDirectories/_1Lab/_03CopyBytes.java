29c512e11c2d2d0c72e460d2f9128c7178f5272b
Georgi Kostadinov <georgi93571@gmail.com>
2023-12-22 20:37:44 +0200

    add solution

---
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_4StreamsFilesAndDirectories/_1Lab/_03CopyBytes.java
@@ -0,0 +1,26 @@
+package _3ProgrammingJavaAdvancedJanuary2024._1JavaAdvanced._4StreamsFilesAndDirectories._1Lab;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class _03CopyBytes {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("D:\\SoftwareUniversity\\Softrware-University\\src\\_3ProgrammingJavaAdvancedJanuary2024\\_1JavaAdvanced\\_4StreamsFilesAndDirectories\\_JavaAdvancedFilesAndStreamsLabResources\\input.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream("D:\\SoftwareUniversity\\Softrware-University\\src\\_3ProgrammingJavaAdvancedJanuary2024\\_1JavaAdvanced\\_4StreamsFilesAndDirectories\\_JavaAdvancedFilesAndStreamsLabResources\\03.CopyBytesOutput.txt");
+        int currentByte = fileInputStream.read();
+        while (currentByte >= 0) {
+            if (currentByte == 32 || currentByte == 10) {
+                fileOutputStream.write(currentByte);
+            } else {
+                String digit = String.valueOf(currentByte);
+                for (int i = 0; i < digit.length(); i++) {
+                    fileOutputStream.write(digit.charAt(i));
+                }
+            }
+            currentByte = fileInputStream.read();
+        }
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+}
