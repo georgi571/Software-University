dba5e42a1b95b794265618c85260c2d44927d03f
Georgi Kostadinov <georgi93571@gmail.com>
2023-12-23 21:45:15 +0200

    add solution

---
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_4StreamsFilesAndDirectories/_2Exercises/_01SumLines.java
@@ -0,0 +1,24 @@
+package _3ProgrammingJavaAdvancedJanuary2024._1JavaAdvanced._4StreamsFilesAndDirectories._2Exercises;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class _01SumLines {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = Files.newBufferedReader(Path.of("D:\\SoftwareUniversity\\Softrware-University\\src\\_3ProgrammingJavaAdvancedJanuary2024\\_1JavaAdvanced\\_4StreamsFilesAndDirectories\\JavaAdvancedFilesAndStreamsExercisesResources\\input.txt"));
+        String line = reader.readLine();
+        while (line != null) {
+            char[] chars = line.toCharArray();
+            long sum = 0;
+            for (char c : chars) {
+                sum += c;
+            }
+            System.out.printf("%d%n", sum);
+            line = reader.readLine();
+        }
+        reader.close();
+    }
+}
