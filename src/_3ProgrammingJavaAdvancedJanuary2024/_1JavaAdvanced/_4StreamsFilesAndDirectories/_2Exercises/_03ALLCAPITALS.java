a965a411718f0786e76a17b0f395e84410267a4b
Georgi Kostadinov <georgi93571@gmail.com>
2023-12-28 19:58:24 +0200

    add solution

---
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_4StreamsFilesAndDirectories/_2Exercises/_03ALLCAPITALS.java
@@ -0,0 +1,22 @@
+package _3ProgrammingJavaAdvancedJanuary2024._1JavaAdvanced._4StreamsFilesAndDirectories._2Exercises;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class _03ALLCAPITALS {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = Files.newBufferedReader(Path.of("D:\\SoftwareUniversity\\Softrware-University\\src\\_3ProgrammingJavaAdvancedJanuary2024\\_1JavaAdvanced\\_4StreamsFilesAndDirectories\\JavaAdvancedFilesAndStreamsExercisesResources\\input.txt"));
+        BufferedWriter writer = Files.newBufferedWriter(Path.of("D:\\SoftwareUniversity\\Softrware-University\\src\\_3ProgrammingJavaAdvancedJanuary2024\\_1JavaAdvanced\\_4StreamsFilesAndDirectories\\JavaAdvancedFilesAndStreamsExercisesResources\\output.txt"));
+        String read = reader.readLine();
+        while (read != null) {
+            writer.write(read.toUpperCase());
+            writer.write(System.lineSeparator());
+            read = reader.readLine();
+        }
+        reader.close();
+        writer.close();
+    }
+}
