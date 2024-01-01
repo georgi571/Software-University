ffb25893855f3e714271137077a1512cbd1d6c11
Georgi Kostadinov <georgi93571@gmail.com>
2024-01-01 23:09:39 +0200

    add solution

---
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_4StreamsFilesAndDirectories/_2Exercises/_08GetFolderSize.java
@@ -0,0 +1,21 @@
+package _3ProgrammingJavaAdvancedJanuary2024._1JavaAdvanced._4StreamsFilesAndDirectories._2Exercises;
+
+import java.io.File;
+
+public class _08GetFolderSize {
+    public static void main(String[] args) {
+        File directory = new File("D:\\SoftwareUniversity\\Softrware-University\\src\\_3ProgrammingJavaAdvancedJanuary2024\\_1JavaAdvanced\\_4StreamsFilesAndDirectories\\JavaAdvancedFilesAndStreamsExercisesResources\\Exercises Resources");
+        long size = 0;
+        if (directory.exists()) {
+            if (directory.isDirectory()) {
+                File[] files = directory.listFiles();
+                for (File file : files) {
+                    if (!file.isDirectory()) {
+                        size += file.length();
+                    }
+                }
+            }
+        }
+        System.out.printf("Folder size: %d", size);
+    }
+}
