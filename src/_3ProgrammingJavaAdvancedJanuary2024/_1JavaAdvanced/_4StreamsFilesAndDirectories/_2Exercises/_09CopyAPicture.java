1a44f1cd8e65b35e1686765586b2f12be4839a56
Georgi Kostadinov <georgi93571@gmail.com>
2024-01-01 23:26:44 +0200

    add solution

---
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_4StreamsFilesAndDirectories/_2Exercises/_09CopyAPicture.java
@@ -0,0 +1,19 @@
+package _3ProgrammingJavaAdvancedJanuary2024._1JavaAdvanced._4StreamsFilesAndDirectories._2Exercises;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class _09CopyAPicture {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("D:\\SoftwareUniversity\\Softrware-University\\src\\_3ProgrammingJavaAdvancedJanuary2024\\_1JavaAdvanced\\_4StreamsFilesAndDirectories\\JavaAdvancedFilesAndStreamsExercisesResources\\Flag_of_Bulgaria.jpg");
+        FileOutputStream fileOutputStream = new FileOutputStream("D:\\SoftwareUniversity\\Softrware-University\\src\\_3ProgrammingJavaAdvancedJanuary2024\\_1JavaAdvanced\\_4StreamsFilesAndDirectories\\JavaAdvancedFilesAndStreamsExercisesResources\\Copy_Flag_of_Bulgaria.jpg");
+        int readByte = fileInputStream.read();
+        while (readByte != -1) {
+            fileOutputStream.write(readByte);
+            readByte = fileInputStream.read();
+        }
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+}
