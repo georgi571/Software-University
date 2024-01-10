dabb67d180d6026b30f00e050593d764fc19a738
Georgi Kostadinov <georgi93571@gmail.com>
2024-01-10 21:46:44 +0200

    add solution

---
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_6DefiningClasses/_2Exersices/_05CarSalesman/Car.java
@@ -0,0 +1,47 @@
+package _3ProgrammingJavaAdvancedJanuary2024._1JavaAdvanced._6DefiningClasses._2Exersices._05CarSalesman;
+
+public class Car {
+    private  String model;
+    private  Engine engine;
+    private  String weight;
+    private  String color;
+
+    public Car(String model, Engine engine, String weight, String color) {
+        this.model = model;
+        this.engine = engine;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_6DefiningClasses/_2Exersices/_05CarSalesman/Engine.java
@@ -0,0 +1,47 @@
+package _3ProgrammingJavaAdvancedJanuary2024._1JavaAdvanced._6DefiningClasses._2Exersices._05CarSalesman;
+
+public class Engine {
+    private String model;
+    private int power;
+    private String displacement;
+    private String efficiency;
+
+    public Engine(String model, int power, String displacement, String efficiency) {
+        this.model = model;
+        this.power = power;
+        this.displacement = displacement;
+        this.efficiency = efficiency;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(String displacement) {
+        this.displacement = displacement;
+    }
+
+    public String getEfficiency() {
+        return efficiency;
+    }
+
+    public void setEfficiency(String efficiency) {
+        this.efficiency = efficiency;
+    }
+}
