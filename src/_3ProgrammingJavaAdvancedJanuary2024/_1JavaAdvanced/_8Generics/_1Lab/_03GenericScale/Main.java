1897552e21c3f6c8045bb5480e412b251b117586
Georgi Kostadinov <georgi93571@gmail.com>
2024-01-19 01:14:27 +0200

    add solution

---
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_8Generics/_1Lab/_03GenericScale/Main.java
@@ -0,0 +1,7 @@
+package _03GenericScale;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_8Generics/_1Lab/_03GenericScale/Scale.java
@@ -0,0 +1,20 @@
+package _03GenericScale;
+
+public class Scale<T extends Comparable<T>> {
+    private T left;
+    private T right;
+
+    public Scale(T left, T right) {
+        this.left = left;
+        this.right = right;
+    }
+    public T getHeavier() {
+        if (this.left.compareTo(this.right) > 0) {
+            return this.left;
+        }
+        if (this.left.compareTo(this.right) < 0) {
+            return this.right;
+        }
+        return null;
+    }
+}
