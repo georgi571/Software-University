22d15d6fedd2b43e33dc497bec40ebb21e3e75fb
Georgi Kostadinov <georgi93571@gmail.com>
2024-01-20 13:13:20 +0200

    add solution

--- src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_8Generics/_1Lab/_03GenericScale/Main.java
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_8Generics/_1Lab/_03GenericScale/Main.java
@@ -1,4 +1,4 @@
-package _03GenericScale;
+package _3ProgrammingJavaAdvancedJanuary2024._1JavaAdvanced._8Generics._1Lab._03GenericScale;
 
 public class Main {
     public static void main(String[] args) {
--- src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_8Generics/_1Lab/_03GenericScale/Scale.java
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_8Generics/_1Lab/_03GenericScale/Scale.java
@@ -1,4 +1,4 @@
-package _03GenericScale;
+package _3ProgrammingJavaAdvancedJanuary2024._1JavaAdvanced._8Generics._1Lab._03GenericScale;
 
 public class Scale<T extends Comparable<T>> {
     private T left;
