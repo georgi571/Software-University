5d7fde30f4120921b3d64bee96e02e18dd92ab45
Georgi Kostadinov <georgi93571@gmail.com>
2024-01-20 21:13:37 +0200

    add solution

---
+++ src/_3ProgrammingJavaAdvancedJanuary2024/_1JavaAdvanced/_9IteratorsAndComparators/_1Lab/_01Book/Main.java
@@ -0,0 +1,11 @@
+package _3ProgrammingJavaAdvancedJanuary2024._1JavaAdvanced._9IteratorsAndComparators._1Lab._01Book;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
