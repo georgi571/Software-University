269fe11464a218de87160c0b3f178f87106a0710
Georgi Kostadinov <georgi93571@gmail.com>
2024-02-07 20:33:15 +0200

    add solution

---
+++ src/_4ProgrammingJavaOOPFebruary2024/_1JavaOOP/_3Inheritance/_1Lab/_02HierarchicalInheritance/Main.java
@@ -0,0 +1,13 @@
+package _4ProgrammingJavaOOPFebruary2024._1JavaOOP._3Inheritance._1Lab._02HierarchicalInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+
+        Cat cat = new Cat();
+        cat.eat();
+        cat.meow();
+    }
+}
