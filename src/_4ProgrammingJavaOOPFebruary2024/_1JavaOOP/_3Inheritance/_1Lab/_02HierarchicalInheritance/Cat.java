c7223dc578fb6aa901e7d3c0d2b28782a9472b6c
Georgi Kostadinov <georgi93571@gmail.com>
2024-02-07 20:33:03 +0200

    add solution

---
+++ src/_4ProgrammingJavaOOPFebruary2024/_1JavaOOP/_3Inheritance/_1Lab/_02HierarchicalInheritance/Cat.java
@@ -0,0 +1,8 @@
+package _4ProgrammingJavaOOPFebruary2024._1JavaOOP._3Inheritance._1Lab._02HierarchicalInheritance;
+
+public class Cat extends Animal {
+
+    public void meow() {
+        System.out.printf("meowing…%n");
+    }
+}
