01a863d5e03048d6fcf3358873f0c5a80027cbd9
Georgi Kostadinov <georgi93571@gmail.com>
2024-02-07 20:30:56 +0200

    add solution

---
+++ src/_4ProgrammingJavaOOPFebruary2024/_1JavaOOP/_3Inheritance/_1Lab/_01SingleInheritance/Animal.java
@@ -0,0 +1,8 @@
+package _4ProgrammingJavaOOPFebruary2024._1JavaOOP._3Inheritance._1Lab._01SingleInheritance;
+
+public class Animal {
+
+    public void eat() {
+        System.out.printf("eating…%n");
+    }
+}
---
+++ src/_4ProgrammingJavaOOPFebruary2024/_1JavaOOP/_3Inheritance/_1Lab/_01SingleInheritance/Main.java
@@ -0,0 +1,9 @@
+package _4ProgrammingJavaOOPFebruary2024._1JavaOOP._3Inheritance._1Lab._01SingleInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
