ddb4254a4be2590c053b57d10f4d6ecbf0ef7882
Georgi Kostadinov <georgi93571@gmail.com>
2024-02-10 09:38:09 +0200

    add solution

---
+++ src/_4ProgrammingJavaOOPFebruary2024/_1JavaOOP/_4InterfacesAndAbstraction/_1Lab/_01CarShop/Seat.java
@@ -0,0 +1,40 @@
+package _4ProgrammingJavaOOPFebruary2024._1JavaOOP._4InterfacesAndAbstraction._1Lab._01CarShop;
+
+public class Seat implements Car {
+    private String model;
+    private String color;
+    private Integer horsePower;
+    private String countryProduced;
+
+    public Seat(String model, String color, Integer horsePower, String countryProduced) {
+        this.model = model;
+        this.color = color;
+        this.horsePower = horsePower;
+        this.countryProduced = countryProduced;
+    }
+
+    @Override
+    public String getModel() {
+        return this.model;
+    }
+
+    @Override
+    public String getColor() {
+        return this.color;
+    }
+
+    @Override
+    public Integer getHorsePower() {
+        return this.horsePower;
+    }
+
+    @Override
+    public String countryProduced() {
+        return this.countryProduced;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("This is %s produced in %s and have %d tires", getModel(), countryProduced(), Car.TIRES);
+    }
+}
