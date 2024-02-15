e33e98610c412bdfaf709a74b40e2737ebb5e6df
Georgi Kostadinov <georgi93571@gmail.com>
2024-02-15 22:47:58 +0200

    add solution

---
+++ src/_4ProgrammingJavaOOPFebruary2024/_1JavaOOP/_6SOLID/_1Lab/_05DependencyInversion/_03Database/Data.java
@@ -0,0 +1,27 @@
+package _4ProgrammingJavaOOPFebruary2024._1JavaOOP._6SOLID._1Lab._05DependencyInversion._03Database;
+
+public class Data {
+    public Iterable<Integer> courseIds()
+    {
+        // return course ids
+        return null;
+    }
+
+    public Iterable<String> courseNames()
+    {
+        // return course names
+        return null;
+    }
+
+    public Iterable<String> search(String substring)
+    {
+        // return found results
+        return null;
+    }
+
+    public String getCourseById(int id)
+    {
+        // return course by id
+        return null;
+    }
+}
