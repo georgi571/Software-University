35306302c16bffc645465626a9604ab100bd66ce
Georgi Kostadinov <georgi93571@gmail.com>
2024-06-21 19:59:50 +0300

    add solution

---
+++ src/_6ProgrammingJSFrontEnd/_1JSFrontEnd/_1JavaScriptSyntaxFundamentals/_2Exercises/_01Ages.js
@@ -0,0 +1,15 @@
+function solve(age) {
+    if (age < 0) {
+        console.log('out of bounds');
+    } else if (age <= 2) {
+        console.log('baby');
+    } else if (age <= 13) {
+        console.log('child');
+    } else if (age <= 19) {
+        console.log('teenager');
+    } else if (age <= 65) {
+        console.log('adult');
+    } else {
+        console.log('elder');
+    }
+}
