e46b7f5a83b141f5ec95ad5048179a311af611c8
Georgi Kostadinov <georgi93571@gmail.com>
2024-07-04 23:46:46 +0300

    add solution

---
+++ src/_6ProgrammingJSFrontEndJune2024/_1JSFrontEnd/_3FunctionsAndStatements/_2Exercises/_07NxNMatrix.js
@@ -0,0 +1,9 @@
+function matrix(number) {
+    let output = '';
+    for (let i = 0; i < number; i++) {
+        output += number + ' ';
+    }
+    for (let i = 0; i < number; i++) {
+        console.log(output);
+    }
+}
