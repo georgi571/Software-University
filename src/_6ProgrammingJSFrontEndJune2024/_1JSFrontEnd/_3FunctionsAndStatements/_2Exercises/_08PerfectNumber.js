71aa5cf24c877a7f1998a6fe516dea26655e4ca0
Georgi Kostadinov <georgi93571@gmail.com>
2024-07-05 00:05:21 +0300

    add solution

---
+++ src/_6ProgrammingJSFrontEndJune2024/_1JSFrontEnd/_3FunctionsAndStatements/_2Exercises/_08PerfectNumber.js
@@ -0,0 +1,16 @@
+function perfectNumber(number) {
+    let sum = 0;
+    for (let i = 1; i < number; i++) {
+        if (number % i === 0) {
+            sum += i;
+        }
+        if (sum > number) {
+            break;
+        }
+    }
+    if (sum === number) {
+        console.log('We have a perfect number!')
+    } else {
+        console.log("It's not so perfect.")
+    }
+}
