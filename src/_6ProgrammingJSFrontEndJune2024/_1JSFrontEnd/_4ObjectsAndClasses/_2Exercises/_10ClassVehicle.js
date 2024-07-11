0fb9f95a97f77fa97ee3762130941eb62e502106
Georgi Kostadinov <georgi93571@gmail.com>
2024-07-11 22:29:24 +0300

    add solution

---
+++ src/_6ProgrammingJSFrontEndJune2024/_1JSFrontEnd/_4ObjectsAndClasses/_2Exercises/_10ClassVehicle.js
@@ -0,0 +1,21 @@
+class Vehicle {
+    type;
+    model;
+    parts = {};
+    fuel;
+
+    constructor(type, model, parts, fuel) {
+        this.type = type;
+        this.model = model;
+        this.parts = {
+            engine: parts.engine,
+            power: parts.power,
+            quality: parts.engine * parts.power,
+        };
+        this.fuel = fuel;
+    }
+
+    drive(amount) {
+        this.fuel -= amount;
+    }
+}
