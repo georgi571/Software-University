c0313ede977a82965533fa59d5f451fc479d62a1
Georgi Kostadinov <georgi93571@gmail.com>
2024-07-18 22:16:37 +0300

    add solution

---
+++ src/_6ProgrammingJSFrontEndJune2024/_1JSFrontEnd/_5DomIntroduction/_1Lab/_03SumNumbers/SumNumbers.js
@@ -0,0 +1,5 @@
+function calc() {
+    const firstNumber = Number(document.getElementById('num1').value);
+    const secondNumber = Number(document.getElementById('num2').value);
+    document.getElementById('sum').value = firstNumber + secondNumber;
+}
