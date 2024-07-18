1b37c3118648d3b716bee3ffd313597672f325a8
Georgi Kostadinov <georgi93571@gmail.com>
2024-07-18 22:20:27 +0300

    add solution

---
+++ src/_6ProgrammingJSFrontEndJune2024/_1JSFrontEnd/_5DomIntroduction/_1Lab/_04ShowMore/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const button = document.getElementById('more');
+    const hiddenText = document.getElementById('text');
+    button.style.display = 'none';
+    hiddenText.style.display = 'inline';
+}
