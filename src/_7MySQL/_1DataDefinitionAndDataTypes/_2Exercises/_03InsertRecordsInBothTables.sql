7a99314bf297b266fca4d9aeb2f7044f99e537da
Georgi Kostadinov <georgi93571@gmail.com>
2024-08-19 07:30:03 +0300

    add solution

---
+++ src/_7MySQL/_1DataDefinitionAndDataTypes/_2Exercises/_03InsertRecordsInBothTables.sql
@@ -0,0 +1,7 @@
+INSERT INTO minions.towns(name) VALUE ('Sofia');
+INSERT INTO minions.towns(name) VALUE ('Plovdiv');
+INSERT INTO minions.towns(name) VALUE ('Varna');
+
+INSERT INTO minions.minions(name, age, town_id) VALUE ('Kevin', 22, 1);
+INSERT INTO minions.minions(name, age, town_id) VALUE ('Bob', 15, 3);
+INSERT INTO minions.minions(name, age, town_id) VALUE ('Steward', NULL, 2);
