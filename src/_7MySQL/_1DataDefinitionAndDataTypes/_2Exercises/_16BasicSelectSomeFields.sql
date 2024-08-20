5cf39b345e5ef955b06e5f823ec9eda300fc2bd9
Georgi Kostadinov <georgi93571@gmail.com>
2024-08-20 10:03:40 +0300

    add solution

---
+++ src/_7MySQL/_1DataDefinitionAndDataTypes/_2Exercises/_16BasicSelectSomeFields.sql
@@ -0,0 +1,3 @@
+SELECT name FROM soft_uni.towns ORDER BY name;
+SELECT name FROM soft_uni.departments ORDER BY name;
+SELECT first_name, last_name, job_title, salary FROM soft_uni.employees ORDER BY salary DESC;
