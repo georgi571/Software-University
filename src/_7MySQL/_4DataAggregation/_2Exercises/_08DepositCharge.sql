13eb9817673df456d210f86f159016964ff77c0e
Georgi Kostadinov <georgi93571@gmail.com>
2024-08-24 18:27:40 +0300

    add solution

---
+++ src/_7MySQL/_4DataAggregation/_2Exercises/_08DepositCharge.sql
@@ -0,0 +1,7 @@
+SELECT
+    deposit_group,
+    magic_wand_creator,
+    MIN(deposit_charge) AS `min_deposit_charge`
+FROM gringotts.wizzard_deposits
+GROUP BY deposit_group, magic_wand_creator
+ORDER BY magic_wand_creator, deposit_group;
