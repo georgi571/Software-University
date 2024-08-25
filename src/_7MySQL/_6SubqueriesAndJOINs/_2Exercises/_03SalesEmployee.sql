76c9baaac4e2828440e4dd90ef00459a24bd4e9c
Georgi Kostadinov <georgi93571@gmail.com>
2024-08-25 18:47:43 +0300

    add solution

---
+++ src/_7MySQL/_6SubqueriesAndJOINs/_2Exercises/_03SalesEmployee.sql
@@ -0,0 +1,9 @@
+SELECT
+    e.employee_id,
+    e.first_name,
+    e.last_name,
+    d.name
+FROM soft_uni6.employees AS e
+    JOIN soft_uni6.departments d on d.department_id = e.department_id
+WHERE d.name = 'Sales'
+ORDER BY e.employee_id DESC;
