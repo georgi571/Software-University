13fcab1bb7bc0112fdd1c87e2b4002ad3e17a4eb
Georgi Kostadinov <georgi93571@gmail.com>
2024-08-25 10:11:58 +0300

    add solution

---
+++ src/_7MySQL/_5TableRelations/_1Lab/_04DeleteMountains.sql
@@ -0,0 +1,15 @@
+CREATE TABLE camp.mountains (
+    id   INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE camp.peaks (
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    mountain_id INT,
+
+    CONSTRAINT fk_peaks_mountains
+        FOREIGN KEY (mountain_id)
+            REFERENCES mountains (id)
+                ON DELETE CASCADE
+);
