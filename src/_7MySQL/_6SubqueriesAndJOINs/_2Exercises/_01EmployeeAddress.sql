b0d08674bf0fdd242a442256c4973a2e4ffa5307
Georgi Kostadinov <georgi93571@gmail.com>
2024-08-25 18:22:04 +0300

    add solution

---
+++ src/_7MySQL/_6SubqueriesAndJOINs/_2Exercises/_01EmployeeAddress.sql
@@ -0,0 +1,8 @@
+SELECT
+    e.employee_id,
+    e.job_title,
+    e.address_id,
+    a.address_text
+FROM soft_uni6.employees AS e
+JOIN soft_uni6.addresses AS a on a.address_id = e.address_id
+ORDER BY a.address_id LIMIT 5;
