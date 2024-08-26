f312826ecd334d0a04ce43ea9b00978d06f16171
Georgi Kostadinov <georgi93571@gmail.com>
2024-08-26 20:30:53 +0300

    add solution

---
+++ src/_7MySQL/_7DatabaseProgrammability/_2Exercises/_10FutureValueFunction.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+CREATE FUNCTION bank.ufn_calculate_future_value(`sum` DECIMAL(19, 4),`yearly_interest_rate` DOUBLE(6,2),`number_of_years` INT)
+    RETURNS DECIMAL(19, 4)
+    DETERMINISTIC
+BEGIN
+    DECLARE future_value DECIMAL(19, 4);
+
+    SET future_value = sum * POWER(1 + yearly_interest_rate, number_of_years);
+
+    RETURN future_value;
+END$$
+DELIMITER ;
