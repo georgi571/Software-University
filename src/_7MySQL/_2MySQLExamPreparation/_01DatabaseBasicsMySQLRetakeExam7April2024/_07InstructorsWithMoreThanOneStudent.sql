3d44fbb28b604ff1e71189f4713f1e57fd8763a8
Georgi Kostadinov <georgi93571@gmail.com>
2024-08-31 00:26:30 +0300

    add solution

---
+++ src/_7MySQL/_2MySQLExamPreparation/_01DatabaseBasicsMySQLRetakeExam7April2024/_07InstructorsWithMoreThanOneStudent.sql
@@ -0,0 +1,13 @@
+SELECT
+    i.first_name,
+    i.last_name,
+    COUNT(*) AS `students_count`,
+    c.name
+FROM go_roadie.instructors_students AS ins
+    JOIN go_roadie.instructors AS i ON i.id = ins.instructor_id
+    JOIN go_roadie.instructors_driving_schools AS ids ON i.id = ids.instructor_id
+    JOIN go_roadie.driving_schools ds on ds.id = ids.driving_school_id
+    JOIN go_roadie.cities c on c.id = ds.city_id
+GROUP BY ins.instructor_id, c.name, i.first_name
+HAVING students_count > 1
+ORDER BY students_count DESC, i.first_name;
