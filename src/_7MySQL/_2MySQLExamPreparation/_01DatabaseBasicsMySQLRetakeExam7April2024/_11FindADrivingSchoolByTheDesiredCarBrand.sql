56becd2baab300594bfadf247fd311adb35c660d
Georgi Kostadinov <georgi93571@gmail.com>
2024-08-31 01:03:30 +0300

    add solution

---
+++ src/_7MySQL/_2MySQLExamPreparation/_01DatabaseBasicsMySQLRetakeExam7April2024/_11FindADrivingSchoolByTheDesiredCarBrand.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+CREATE PROCEDURE go_roadie.udp_find_school_by_car(brand VARCHAR(20))
+BEGIN
+    SELECT
+        ds.name,
+        ds.average_lesson_price
+    FROM go_roadie.driving_schools AS ds
+             JOIN go_roadie.cars c on c.id = ds.car_id
+    WHERE c.brand = `brand`
+    ORDER BY ds.average_lesson_price DESC;
+END$$
+DELIMITER ;
+
+
