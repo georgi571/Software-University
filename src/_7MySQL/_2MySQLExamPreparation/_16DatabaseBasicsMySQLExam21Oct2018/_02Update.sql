f1ced9d5c260c83491e183dbf95718448c2e6cf6
Georgi Kostadinov <georgi93571@gmail.com>
2024-09-01 16:44:36 +0300

    add solution

---
+++ src/_7MySQL/_2MySQLExamPreparation/_16DatabaseBasicsMySQLExam21Oct2018/_02Update.sql
@@ -0,0 +1,8 @@
+UPDATE colonial_journey_management_system.journeys
+SET purpose = CASE
+        WHEN id % 2 = 0 THEN 'Medical'
+        WHEN id % 3 = 0 THEN 'Technical'
+        WHEN id % 5 = 0 THEN 'Educational'
+        WHEN id % 7 = 0 THEN 'Military'
+        ELSE purpose
+END;
