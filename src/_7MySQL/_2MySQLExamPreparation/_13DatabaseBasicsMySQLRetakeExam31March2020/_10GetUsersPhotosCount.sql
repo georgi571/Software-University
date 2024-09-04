52791f53ad7ba15bf1e90278c563718699cc241f
Georgi Kostadinov <georgi93571@gmail.com>
2024-09-04 19:24:00 +0300

    add solution

---
+++ src/_7MySQL/_2MySQLExamPreparation/_13DatabaseBasicsMySQLRetakeExam31March2020/_10GetUsersPhotosCount.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+CREATE FUNCTION instd.udf_users_photos_count(username VARCHAR(30))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE number_of_photos_that_user_has_upload INT;
+
+    SELECT
+         COUNT(up.photo_id) INTO number_of_photos_that_user_has_upload
+    FROM instd.users AS u
+        LEFT JOIN instd.users_photos up on u.id = up.user_id
+    WHERE u.username = `username`
+    GROUP BY u.id;
+
+    RETURN number_of_photos_that_user_has_upload;
+END$$
+DELIMITER ;
