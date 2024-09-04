c2dd873d0f00538b03dba3015bed317344f6d6c2
Georgi Kostadinov <georgi93571@gmail.com>
2024-09-04 17:12:39 +0300

    add solution

---
+++ src/_7MySQL/_2MySQLExamPreparation/_08DatabaseBasicsMySQLRegularExam13February2022/_10ExtractClientCardsCount.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+CREATE FUNCTION online_store2.udf_customer_products_count(name VARCHAR(30))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE total_number_of_product_a_customer_order INT;
+
+    SELECT
+        COUNT(*) INTO total_number_of_product_a_customer_order
+    FROM online_store2.orders AS o
+             LEFT JOIN online_store2.customers c on c.id = o.customer_id
+             LEFT JOIN online_store2.orders_products op on o.id = op.order_id
+    WHERE c.first_name = `name`;
+
+    RETURN total_number_of_product_a_customer_order;
+END$$
+DELIMITER ;
