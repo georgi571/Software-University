69b84f62a7c0300998439d9c0a6c19026a59a76f
Georgi Kostadinov <georgi93571@gmail.com>
2024-09-23 13:13:53 +0300

    add solution

---
+++ src/_1ProgrammingBasicsWithJavaMay2023/_3ConditionalStatementsAdvanced/_3MoreExercise/_09NumbersFrom1To10.java
@@ -0,0 +1,11 @@
+package _1ProgrammingBasicsWithJavaMay2023._3ConditionalStatementsAdvanced._3MoreExercise;
+
+public class _09NumbersFrom1To10 {
+    public static void main(String[] args) {
+        int number = 1;
+        while (number <= 10) {
+            System.out.printf("%d%n", number);
+            number++;
+        }
+    }
+}
