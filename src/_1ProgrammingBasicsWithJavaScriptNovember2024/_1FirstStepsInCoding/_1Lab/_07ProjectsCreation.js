b0720a2d3646cc84127ed4db7dfa03e0b532498c
Georgi Kostadinov <georgi93571@gmail.com>
2024-11-02 17:45:02 +0200

    add solution

---
+++ src/_1ProgrammingBasicsWithJavaScriptNovember2024/_1FirstStepsInCoding/_1Lab/_07ProjectsCreation.js
@@ -0,0 +1,6 @@
+function projectCreation(name, numberOfProjects) {
+    let timeNeededForOneProject = 3;
+    let totalTimeToFinishAllProjects = numberOfProjects * timeNeededForOneProject;
+
+    console.log(`The architect ${name} will need ${totalTimeToFinishAllProjects} hours to complete ${numberOfProjects} project/s.`);
+}
