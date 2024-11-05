995fb51d7d3e077e50d7f35cbad6b68b73adc392
Georgi Kostadinov <georgi93571@gmail.com>
2024-11-05 07:43:38 +0200

    add solution

---
+++ src/_1ProgrammingBasicsWithJavaScriptNovember2024/_1FirstStepsInCoding/_3MoreExercise/_05TrainingLab.js
@@ -0,0 +1,12 @@
+function trainingLab(w, h) {
+    let wInCm = w * 100;
+    let hInCm = h * 100;
+    let hMinusCorridor = hInCm - 100.00;
+    let deskInRow = Math.floor(hMinusCorridor / 70);
+    let rows = Math.floor(wInCm / 120);
+    let numbersOfChairs = (deskInRow * rows) - 3;
+    console.log(`${numbersOfChairs}`)
+}
+
+trainingLab(15, 8.9)
+trainingLab(8.4, 5.2)
