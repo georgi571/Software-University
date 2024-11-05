60eb5ee300325a5e438df834e2f7232aacd05686
Georgi Kostadinov <georgi93571@gmail.com>
2024-11-05 07:50:46 +0200

    add solution

---
+++ src/_1ProgrammingBasicsWithJavaScriptNovember2024/_1FirstStepsInCoding/_3MoreExercise/_08CircleAreaAndPerimeter.js
@@ -0,0 +1,8 @@
+function circleAreaAndPerimeter(r) {
+    const area = Math.PI * r * r;
+    const perimeter = Math.PI * 2 * r;
+
+    console.log(`${area.toFixed(2)}`)
+    console.log(`${perimeter.toFixed(2)}`)
+}
+
