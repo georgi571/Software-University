44cc894f09968331268a7f2de06877ebb829cc66
Georgi Kostadinov <georgi93571@gmail.com>
2024-11-06 23:46:18 +0200

    add solution

---
+++ src/_1ProgrammingBasicsWithJavaScriptNovember2024/_2ConditionalStatements/_1Lab/_05Number100To200.js
@@ -0,0 +1,9 @@
+function number100To200(number) {
+    if (number < 100) {
+        console.log("Less than 100");
+    } else if (number <= 200) {
+        console.log("Between 100 and 200");
+    } else if (number > 200) {
+        console.log("Greater than 200");
+    }
+}
