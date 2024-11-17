b0731a652b257b60c2026b03a5f3098a69e80ffd
Georgi Kostadinov <georgi93571@gmail.com>
2024-11-17 09:06:59 +0200

    add solution

---
+++ src/_1ProgrammingBasicsWithJavaScriptNovember2024/_3ConditionalStatementsAdvanced/_1Lab/_01DayOfWeek.js
@@ -0,0 +1,32 @@
+function dayOfWeek(number) {
+    let day = "";
+
+    switch (number) {
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        case 7:
+            day = "Sunday";
+            break;
+        default:
+            day = "Error";
+            break;
+    }
+
+    console.log(`${day}`)
+}
