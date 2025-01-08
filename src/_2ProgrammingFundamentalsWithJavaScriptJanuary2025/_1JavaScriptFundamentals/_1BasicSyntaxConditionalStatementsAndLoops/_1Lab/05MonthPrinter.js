04866c7d086b8ad3324a6660f6101ea495f6784b
Georgi Kostadinov <georgi93571@gmail.com>
2025-01-08 19:32:30 +0200

    add solution

---
+++ src/_2ProgrammingFundamentalsWithJavaScriptJanuary2025/_1JavaScriptFundamentals/_1BasicSyntaxConditionalStatementsAndLoops/_1Lab/05MonthPrinter.js
@@ -0,0 +1,29 @@
+function monthPrinter(n) {
+    if (n === 1) {
+        console.log('January');
+    } else if (n === 2) {
+        console.log('February');
+    } else if (n === 3) {
+        console.log('March');
+    } else if (n === 4) {
+        console.log('April');
+    } else if (n === 5) {
+        console.log('May');
+    } else if (n === 6) {
+        console.log('June');
+    } else if (n === 7) {
+        console.log('July');
+    } else if (n === 8) {
+        console.log('August');
+    } else if (n === 9) {
+        console.log('September');
+    } else if (n === 10) {
+        console.log('October');
+    } else if (n === 11) {
+        console.log('November');
+    } else if (n === 12) {
+        console.log('December');
+    } else {
+        console.log('Error!');
+    }
+}
