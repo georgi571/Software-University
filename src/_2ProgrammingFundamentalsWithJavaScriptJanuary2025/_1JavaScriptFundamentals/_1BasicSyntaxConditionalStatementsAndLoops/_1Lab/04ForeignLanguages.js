18fc5f89265c01a82196e7304a77e6b186eecf95
Georgi Kostadinov <georgi93571@gmail.com>
2025-01-08 19:29:10 +0200

    add solution

---
+++ src/_2ProgrammingFundamentalsWithJavaScriptJanuary2025/_1JavaScriptFundamentals/_1BasicSyntaxConditionalStatementsAndLoops/_1Lab/04ForeignLanguages.js
@@ -0,0 +1,9 @@
+function foreignLanguage(country) {
+    if (country === "England" || country === "USA") {
+        console.log('English');
+    } else if (country === "Spain" || country === "Argentina" || country === "Mexico") {
+        console.log('Spanish');
+    } else {
+        console.log('unknown');
+    }
+}
